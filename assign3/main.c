426be746d7d73bcd405a17cabeb13f47bca93616
unknown <connorjstack.wrk@gmail.com>
2023-10-10 16:01:27 -0500

    setting up new assignment 3, linked list project with PCB and threads. Running basic main, and starting the skeleton of the linked list implementation

---
+++ assign3/main.c
@@ -0,0 +1,14 @@
+/*
+*File: 
+*Connor Stack
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "linkedlist.h"
+
+int main(int argc, char *argv[]){
+
+    printf("blah blah");
+    return 0;
+}
