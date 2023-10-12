c9f4e2d4b3dabe154065df1ff7d0ffe1d5d2e2e4
unknown <connorjstack.wrk@gmail.com>
2023-10-11 22:52:12 -0500

    First file read thread is working at a very basic level, reading from file input, and writing lines to standard output

--- assign3/main.c
+++ assign3/main.c
@@ -1,29 +1,69 @@
 /*
-*File: 
-*Connor Stack
-*/
+ *File:
+ *Connor Stack
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include "readyQ.h"
 #include "IOQ.h"
+void *file_reading_thread(void *arg);
 
-int main(int argc, char *argv[]){
-    char* schedule_type = argv[2];
-    char* file_input = NULL;
+int main(int argc, char *argv[])
+{
+    pthread_t tid_file_reader, tid_readyq, tid_ioq;
+    int thread_status;
+    char *schedule_type = argv[2];
+    char *filename = NULL;
+    //temporary
+    filename = argv[1];
+    if (argc == 5)
+    {
+        filename = argv[4];
 
-    if(argc == 5){ 
-        file_input = argv[4];
-
-
-        printf("5");
-    }else if(argc == 7){
-        file_input = argv[6];
-
-        
-        printf("7");
-    }else{
-        printf("invalid number of arguments");
+        printf("5\n");
     }
+    else if (argc == 7)
+    {
+        filename = argv[6];
+
+        printf("7\n");
+    }
+    else
+    {
+        // printf("invalid number of arguments");
+    }
+
+    // ready_Q readyq;
+
+    pthread_create(&tid_file_reader, NULL, file_reading_thread, (void *)filename);
+    pthread_join(tid_file_reader, (void**)&thread_status);
+
     return 0;
 }
+
+
+void *file_reading_thread(void *arg)
+{
+    char *filename = (char *)arg;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        perror("could not open file");
+        return NULL;
+    }
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        // Process the data as needed
+        printf("Read: %s", buffer);
+    }
+
+    // Close the file
+    fclose(file);
+
+    return NULL;
+}
+
+//
