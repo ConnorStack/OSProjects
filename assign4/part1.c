ef28f5780ed102d0d1f2745f447b717376bcf532
ConnorStack <connor.stack@g.austincc.edu>
2023-11-05 13:18:41 -0600

    Reading the given binary files, but the output is larger than described. Need to figure out why

--- assign4/part1.c
+++ assign4/part1.c
@@ -3,5 +3,38 @@
 
 int main(int argc, char *argv[]){
     printf("Hello world\n");
+    // FILE * fptr = fopen("part1testsequence", "rb");
+    FILE * fptr = fopen("part1sequence", "rb");
+    if(fptr == NULL){
+        perror("could not open file\n");
+    }
+
+    unsigned char buffer[1024];
+    size_t buffer_size = sizeof(buffer);
+
+    while(1){
+        // size_t bytes_read = fread(buffer, 1, sizeof(buffer), fptr);
+        unsigned long bytes_read = (unsigned long)fread(buffer, 1, sizeof(buffer), fptr);
+
+        if(bytes_read == 0){
+            if(feof(fptr)){
+                break;
+            }else{
+                perror("error reading file\n");
+                break;
+            }
+        }
+
+        size_t num_ulongs = bytes_read / sizeof(unsigned long);
+        unsigned long *ulongs = (unsigned long *)buffer;
+
+        for(size_t i = 0; i < bytes_read; i++){
+            printf("0x%lx\n", ulongs[i]);
+        }
+        
+    }
+
+    fclose(fptr);
+
     return 0;
 }   
