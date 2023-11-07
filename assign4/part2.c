1c9c4811e4d63cb6941db5487661d999278f3cd6
unknown <connorjstack.wrk@gmail.com>
2023-11-06 21:45:38 -0600

    starting part2.c

--- assign4/part2.c
+++ assign4/part2.c
@@ -8,6 +8,15 @@ typedef struct page_table_cell{
 
 int main(int argc, char *argv[])
 {
+    unsigned long LA, PA;
+
+    unsigned int p=5, f=3, d=7;
+    unsigned int pnum, fnum, dnum;
+    int CLK = 0;
+    int freeframes[8] = {0, 1, 1, 1, 1, 1, 1, 1};
+    int LRUcount[8] = {0};
+    int revmap[8] = {-1};
+    
     printf("working\n");
     return 0;
 }
