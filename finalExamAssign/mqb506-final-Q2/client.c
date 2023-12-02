b75d43108deeb0b1303b613d7c98a89cb256f725
unknown <connorjstack.wrk@gmail.com>
2023-12-02 15:59:00 -0600

    working on problem 2

--- finalExamAssign/mqb506-final-Q2/client.c
+++ finalExamAssign/mqb506-final-Q2/client.c
@@ -7,80 +7,114 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 
-main(argc, argv)
-     int     argc;
-     char    *argv[];
-{ 
-  struct  sockaddr_in sad; /* structure to hold an IP address     */
-  int     clientSocket;    /* socket descriptor                   */ 
-  struct  hostent  *ptrh;  /* pointer to a host table entry       */
-  
-  char    *host;           /* pointer to host name                */
-  int     port;            /* protocol port number                */  
-  
-  char    Sentence[128]; 
-  char    modifiedSentence[128]; 
-  char    buff[128];
-  int     n;
-  
-  if (argc != 3) {
-    fprintf(stderr,"Usage: %s server-name port-number\n",argv[0]);
+main(argc, argv) int argc;
+char *argv[];
+{
+  struct sockaddr_in sad; /* structure to hold an IP address     */
+  int clientSocket;       /* socket descriptor                   */
+  struct hostent *ptrh;   /* pointer to a host table entry       */
+
+  char *host; /* pointer to host name                */
+  int port;   /* protocol port number                */
+
+  char Sentence[128];
+  char modifiedSentence[128];
+  char buff[128];
+  //--
+  char message[128];
+  char response[128];
+  //--
+  int n;
+
+  if (argc != 3)
+  {
+    fprintf(stderr, "Usage: %s server-name port-number\n", argv[0]);
     exit(1);
   }
-  
+
   /* Extract host-name from command-line argument */
-  host = argv[1];         /* if host argument specified   */
-  
+  host = argv[1]; /* if host argument specified   */
+
   /* Extract port number  from command-line argument */
-  port = atoi(argv[2]);   /* convert to binary            */
-  
+  port = atoi(argv[2]); /* convert to binary            */
+
   /* Create a socket. */
-  
+
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  if (clientSocket < 0) {
+  if (clientSocket < 0)
+  {
     fprintf(stderr, "socket creation failed\n");
     exit(1);
   }
 
   /* Connect the socket to the specified server. */
 
-  memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
-  sad.sin_family = AF_INET;           /* set family to Internet     */
+  memset((char *)&sad, 0, sizeof(sad)); /* clear sockaddr structure */
+  sad.sin_family = AF_INET;             /* set family to Internet     */
   sad.sin_port = htons((u_short)port);
   ptrh = gethostbyname(host); /* Convert host name to equivalent IP address and copy to sad. */
-  if ( ((char *)ptrh) == NULL ) {
-    fprintf(stderr,"invalid host: %s\n", host);
+  if (((char *)ptrh) == NULL)
+  {
+    fprintf(stderr, "invalid host: %s\n", host);
     exit(1);
   }
   memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
-  
-  if (connect(clientSocket, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
-    fprintf(stderr,"connect failed\n");
+
+  if (connect(clientSocket, (struct sockaddr *)&sad, sizeof(sad)) < 0)
+  {
+    fprintf(stderr, "connect failed\n");
     exit(1);
   }
-  
-  /* Read a sentence from user */
-  
-  printf("Sentence   :   "); gets(Sentence);
-  
-  /* Send the sentence to the server  */
-  
-  write(clientSocket, Sentence, strlen(Sentence)+1);
-  
-  /* Get the modified sentence from the server and write it to the screen*/
-  modifiedSentence[0]='\0';
-  n=read(clientSocket, buff, sizeof(buff));
-  while(n > 0){
-    strncat(modifiedSentence,buff,n);
-    if (buff[n-1]=='\0') break;
-    n=read(clientSocket, buff, sizeof(buff));
-  }
-  
-  printf("Modified to -> %s\n",modifiedSentence);
 
-  /* Close the socket. */
-  
+  while (1)
+  {
+    // Get user input for the message to send
+    printf("Enter a command ('GET_GRADE ID', 'GET_MAX', 'GET_MIN', 'GET_AVG', 'STOP'): ");
+    fgets(message, 128, stdin); //hard coding 128, should be a const
+
+    // Remove newline character from the message
+    message[strcspn(message, "\n")] = '\0';
+
+    // Send the message to the server
+    write(clientSocket, message, strlen(message) + 1);
+
+    // Break the loop if the message is "STOP"
+    if (strcmp(message, "STOP") == 0)
+    {
+      break;
+    }
+
+    // Receive and print the response from the server
+    n = read(clientSocket, response, sizeof(response));
+    if (n <= 0)
+    {
+      fprintf(stderr, "Error receiving response from server\n");
+      break;
+    }
+    printf("Server Response: %s\n", response);
+  }
+  // /* Read a sentence from user */
+
+  // printf("Sentence   :   "); gets(Sentence);
+
+  // /* Send the sentence to the server  */
+
+  // write(clientSocket, Sentence, strlen(Sentence)+1);
+
+  // /* Get the modified sentence from the server and write it to the screen*/
+  // modifiedSentence[0]='\0';
+  // n=read(clientSocket, buff, sizeof(buff));
+  // while(n > 0){
+  //   strncat(modifiedSentence,buff,n);
+  //   if (buff[n-1]=='\0') break;
+  //   n=read(clientSocket, buff, sizeof(buff));
+  // }
+
+  // printf("Modified to -> %s\n",modifiedSentence);
+
+  // /* Close the socket. */
+
   close(clientSocket);
-  
 }
--- finalExamAssign/mqb506-final-Q2/server.c
+++ finalExamAssign/mqb506-final-Q2/server.c
@@ -7,93 +7,193 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
 
-main(argc, argv)
-     int     argc;
-     char    *argv[];
+struct student_info
 {
-  struct  sockaddr_in sad; /* structure to hold server's address  */
-  int     port;            /* protocol port number                */
-  
-  struct  sockaddr_in cad; /* structure to hold client's address  */
-  int     alen;            /* length of address                   */
-  
-  int     welcomeSocket, connectionSocket; /* socket descriptors  */
-  
-  char    clientSentence[128]; 
-  char    capitalizedSentence[128]; 
-  char    buff[128];
-  int     i, n;
-  
+  char ID[7];
+  char name[20];
+  double grade;
+};
+
+main(argc, argv) int argc;
+char *argv[];
+{
+  struct sockaddr_in sad; /* structure to hold server's address  */
+  int port;               /* protocol port number                */
+
+  struct sockaddr_in cad; /* structure to hold client's address  */
+  int alen;               /* length of address                   */
+
+  int welcomeSocket, connectionSocket; /* socket descriptors  */
+
+  char clientSentence[128];
+  char capitalizedSentence[128];
+  char buff[128];
+  int i, n;
+
+  struct student_info DB[10] = {
+      {"abc123", "Turgay", 99.0},
+      {"def456", "Alice", 87.5},
+      {"ghi789", "Bob", 92.3},
+      {"jkl123", "Charlie", 78.9},
+      {"mno456", "Diana", 94.7},
+      {"pqr789", "Eva", 85.2},
+      {"stu123", "Frank", 88.6},
+      {"vwx456", "Grace", 91.8},
+      {"yza789", "Hank", 79.4},
+      {"bcd123", "Ivy", 96.5}};
+
   /* Check command-line argument for protocol port and extract   */
   /* port number if one is specified. Otherwise, give error      */
-  if (argc > 1) {                /* if argument specified        */
-    port = atoi(argv[1]);        /* convert argument to binary   */
-  } else { 
-    fprintf(stderr,"Usage: %s port-number\n",argv[0]);
+  if (argc > 1)
+  {                       /* if argument specified        */
+    port = atoi(argv[1]); /* convert argument to binary   */
+  }
+  else
+  {
+    fprintf(stderr, "Usage: %s port-number\n", argv[0]);
     exit(1);
   }
-  
+
   /* Create a socket */
 
   welcomeSocket = socket(PF_INET, SOCK_STREAM, 0); /* CREATE SOCKET */
-  if (welcomeSocket < 0) {
+  if (welcomeSocket < 0)
+  {
     fprintf(stderr, "socket creation failed\n");
     exit(1);
   }
-  
+
   /* Bind a local address to the socket */
-  
-  memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure   */
-  sad.sin_family = AF_INET;           /* set family to Internet     */
-  sad.sin_addr.s_addr = INADDR_ANY;   /* set the local IP address   */
-  sad.sin_port = htons((u_short)port);/* set the port number        */ 
-  
-  if (bind(welcomeSocket, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
-    fprintf(stderr,"bind failed\n");
+
+  memset((char *)&sad, 0, sizeof(sad)); /* clear sockaddr structure   */
+  sad.sin_family = AF_INET;             /* set family to Internet     */
+  sad.sin_addr.s_addr = INADDR_ANY;     /* set the local IP address   */
+  sad.sin_port = htons((u_short)port);  /* set the port number        */
+
+  if (bind(welcomeSocket, (struct sockaddr *)&sad, sizeof(sad)) < 0)
+  {
+    fprintf(stderr, "bind failed\n");
     exit(1);
   }
-  
+
   /* Specify the size of request queue */
-  
-  if (listen(welcomeSocket, 10) < 0) {
-    fprintf(stderr,"listen failed\n");
+
+  if (listen(welcomeSocket, 10) < 0)
+  {
+    fprintf(stderr, "listen failed\n");
     exit(1);
   }
-  
+
   /* Main server loop - accept and handle requests */
-  
-  while (1) {
+
+  while (1)
+  {
     alen = sizeof(cad);
-    if ( (connectionSocket=accept(welcomeSocket, (struct sockaddr *)&cad, &alen)) < 0) {
+    if ((connectionSocket = accept(welcomeSocket, (struct sockaddr *)&cad, &alen)) < 0)
+    {
       fprintf(stderr, "accept failed\n");
       exit(1);
     }
-    
+
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+      // Handle fork error
+    }
+    else if (pid == 0)
+    {
+      // Child process
+      close(welcomeSocket); // Child doesn't need the welcome socket
+
+      // Process client requests
+      while (1)
+      {
+        // Receive message from the client
+        n = read(connectionSocket, buff, sizeof(buff));
+        buff[n] = '\0'; // Ensure null-termination
+        if (strcmp(buff, "GET_GRADE ID") == 0)
+        {
+          printf("Getting grade id\n");
+          // Process "GET_GRADE ID"
+          // Look up student in DB and send reply
+        }
+        else if (strcmp(buff, "GET_MAX") == 0)
+        {
+          printf("Getting max\n");
+          // Process "GET_MAX"
+          // Calculate maximum grade and send reply
+        }
+        else if (strcmp(buff, "GET_MIN") == 0)
+        {
+          printf("Getting min\n");
+          // Process "GET_MIN"
+          // Calculate minimum grade and send reply
+        }
+        else if (strcmp(buff, "GET_AVG") == 0)
+        {
+          printf("Getting avg\n");
+          // Process "GET_AVG"
+          // Calculate average grade and send reply
+        }
+        else if (strcmp(buff, "STOP") == 0)
+        {
+          printf("Stopping\n");
+          // Process "STOP"
+          // Close connection and terminate
+          close(connectionSocket);
+          exit(0);
+        }
+        else
+        {
+          printf("unknown message\n");
+        }
+
+        // Send the appropriate response back to the client
+        // Continue this loop until "STOP" is received
+      }
+
+      // Close the connection socket in the child process
+      close(connectionSocket);
+
+      // Terminate the child process
+      exit(0);
+    }
+    else
+    {
+      // Parent process
+      close(connectionSocket); // Parent doesn't need the connection socket
+    }
     /* Get the sentence from the client */
-    
-    clientSentence[0]='\0';
-    n=read(connectionSocket,buff,sizeof(buff));
-    while(n > 0){
-      strncat(clientSentence,buff,n);
-      if (buff[n-1]=='\0') break;
-      n=read(connectionSocket,buff,sizeof(buff));
-    }
-    
-    /* Capitalize the sentence */
-    
-    for(i=0; i <= strlen(clientSentence); i++){
-      if((clientSentence[i] >= 'a') && (clientSentence[i] <= 'z'))
-	capitalizedSentence[i] = clientSentence[i] + ('A' - 'a');
-      else
-	capitalizedSentence[i] = clientSentence[i];
-    }
-    
+
+    // clientSentence[0] = '\0';
+    // n = read(connectionSocket, buff, sizeof(buff));
+    // while (n > 0)
+    // {
+    //   strncat(clientSentence, buff, n);
+    //   if (buff[n - 1] == '\0')
+    //     break;
+    //   n = read(connectionSocket, buff, sizeof(buff));
+    // }
+
+    // /* Capitalize the sentence */
+
+    // for (i = 0; i <= strlen(clientSentence); i++)
+    // {
+    //   if ((clientSentence[i] >= 'a') && (clientSentence[i] <= 'z'))
+    //     capitalizedSentence[i] = clientSentence[i] + ('A' - 'a');
+    //   else
+    //     capitalizedSentence[i] = clientSentence[i];
+    // }
+
     /* send it to the client */
-    
-    write(connectionSocket, capitalizedSentence, strlen(capitalizedSentence)+1);
-    
+
+    write(connectionSocket, capitalizedSentence, strlen(capitalizedSentence) + 1);
+
     close(connectionSocket);
   }
-}
 
+  return 0;
+}
